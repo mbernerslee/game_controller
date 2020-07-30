4c5fff48ba3bc80d89494e85ae6fd89dce7aceeb
berners <matthewbernerslee@gmail.com>
2020-07-30 02:50:14 +0100

    removing TODO

--- lib/game_controller_web/controllers/test_logins_controller.ex
+++ lib/game_controller_web/controllers/test_logins_controller.ex
@@ -9,8 +9,6 @@ defmodule GameControllerWeb.TestLoginsController do
   def reseed(conn, _params) do
     Seeds.insert()
 
-    IO.inspect("I reset the seed data")
-
     conn
     |> put_flash(:info, "Test logins (seed data) has been reset")
     |> redirect(to: Routes.test_logins_path(conn, :show))
