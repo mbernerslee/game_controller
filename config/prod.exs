97b111b423ef2dadf1e771ec7ecdfc8918e4112c
Debian <admin@ip-172-31-4-183.eu-west-2.compute.internal>
2020-08-03 08:07:27 +0000

    adding live view signing salt to prod

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :game_controller, GameControllerWeb.Endpoint,
   url: [host: "35.179.21.114", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
 
 # Do not print debug messages in production
 config :logger, level: :info
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -23,12 +23,20 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
+live_view_signing_salt =
+  System.get_env("LIVE_VIEW_SIGNING_SALT") ||
+    raise """
+    environment variable LIVE_VIEW_SIGNING_SALT is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
 config :game_controller, GameControllerWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
+  live_view: [signing_salt: live_view_signing_salt]
 
 # ## Using releases (Elixir v1.9+)
 #
